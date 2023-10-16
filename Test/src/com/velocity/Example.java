09a18652f5f452227a0444ddc8b480491b40d8c4
unknown <sarveshnandurkar107@gmail.com>
2023-10-16 00:42:21 -0700

    addition code is added

---
+++ Test/src/com/velocity/Example.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b= 30;
+		int c=a+b;
+		System.out.println("Addition is>>  "+c);
+	}
+
+}
